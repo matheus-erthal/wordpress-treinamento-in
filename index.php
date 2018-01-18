fd439c2eb8a7e85bd430239295547b4861b1c9ba
Matheus Erthal <matheusamancio@id.uff.br>
2018-01-18 13:47:46 +0000

    Adicionando o The Loop

--- index.php
+++ index.php
@@ -9,6 +9,14 @@
 
 get_header(); ?>
 
-<h1>Amanco e Bruna Rules</h1>
+<?php if (have_posts()): ?>
+    <?php while(have_posts()): ?>
+        <?php the_post(); ?>
+        <h2><?php the_title(); ?></h2>
+    <?php endwhile; ?>
+<?php endif; ?>
+
+
+
 
 <?php get_footer();
