01f2b99d90e410155060983d9fb7dae040d59493
Pedro de Souza e Silva <pedrosouzaesilva@id.uff.br>
2018-01-18 15:49:05 -0200

    Add Arq: Aula Wordpress 1

--- functions.php
+++ functions.php
@@ -1,3 +1,36 @@
 <?php
 
+add_theme_support( 'post-thumbnails' );
 
+function cadastrando_post_type_news() {
+
+    $nomeSingular = 'Notícia';
+    $nomePlural = 'Notícias';
+    $descricao = 'Notícias da IN';
+
+    $labels = array(
+        'name' => $nomePlural,
+        'name_singular' => $nomeSingular,
+        'add_new_item' => 'Adicionar nova ' . $nomeSingular,
+        'edit_item' => 'Editar ' . $nomeSingular
+    );
+
+    $supports = array(
+        'title',
+        'editor',
+        'thumbnail'
+    );
+
+    $args = array(
+        'labels' => $labels,
+        'description' => $descricao,
+        'public' => true,
+        'menu_icon' => 'dashicons-edit',
+        'supports' => $supports
+    );
+
+
+    register_post_type( 'news', $args);
+}
+
+add_action('init', 'cadastrando_post_type_news');
--- single.php
+++ single.php
@@ -12,4 +12,10 @@
 
 get_header(); ?>
 
+<?php while(have_posts()):  the_post();?>:
+  <h2><?php the_title();?></h2>
+  <h3><?php the_content();?></h3>
+
+<?php endwhile; ?>
+
 <?php get_footer();
