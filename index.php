122fd336797822441ffc8dc424e4520accb5f3ca
Matheus Erthal <matheusamancio@id.uff.br>
2018-01-18 15:47:02 +0000

    Nova single

--- index.php
+++ index.php
@@ -12,7 +12,8 @@ get_header(); ?>
 <?php if (have_posts()): ?>
     <?php while(have_posts()): ?>
         <?php the_post(); ?>
-        <h2><?php the_title(); ?></h2>
+        <a href="<?php get_permalink(); ?>"><h2><?php the_title(); ?></h2></a>
+        
     <?php endwhile; ?>
 <?php endif; ?>
 
