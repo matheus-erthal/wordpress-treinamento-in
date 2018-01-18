122afdb678cc29ba090417470546ae79f0906820
Beatriz Tantow <beatriz.tantow@gmail.com>
2018-01-18 17:30:09 -0200

    Aula 18/01.

--- index.php
+++ index.php
@@ -9,14 +9,24 @@
 
 get_header(); ?>
 
-<?php if (have_posts()): ?>
-    <?php while(have_posts()): ?>
-        <?php the_post(); ?>
-        <h2><?php the_title(); ?></h2>
+<?php //the Query - exibe 2 posts por página.
+    $nossa_query = new WP_Query( array('posts_per_page' => 2, order => 'DESC'));
+ ?>
+
+ <?php //the Query - exibe 2 posts por página.
+    $nossa_query = new WP_Query( array('post_type' => notas));
+ ?>
+
+ 
+<?php if ($nossa_query->have_posts()): ?>
+    <?php while($nossa_query->have_posts()): ?>
+        <?php $nossa_query->the_post(); ?>
+        <a href="<?php get_permalink()?>"><?php the_title();?><h2><?php the_title(); ?></h2></a>
+        
     <?php endwhile; ?>
 <?php endif; ?>
 
-
+<?php  ?>
 
 
 <?php get_footer();
