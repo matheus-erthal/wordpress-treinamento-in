78e107e968d4328f0fe4b026dd422f1609717198
Beatriz Tantow <beatriz.tantow@gmail.com>
2018-01-19 11:06:02 -0200

    criacao da pagina sobre.

---
+++ sobre.php
@@ -0,0 +1,14 @@
+<?php /* Template Name: Sobre */ ?>
+<?php
+/**
+ *
+ * @package WordPress
+ * @subpackage meu-tema
+ * @since 1.0
+ * @version 1.0
+ */
+
+get_header(); ?>
+
+
+<?php get_footer();
