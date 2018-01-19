da60a4aa888c5bf1f9bee6061d100d2bb315161a
ThalesMachado <thalesm@id.uff.br>
2018-01-19 16:54:00 -0200

    Estilização do HTML da quem somos e correção de tags não fechadas no header

--- quem-somos.php
+++ quem-somos.php
@@ -3,8 +3,12 @@
 get_header(); ?>
 <section class="container">
 <div class="container-fotos">
-<!-- Apenas para testar se o cacete da imagem vai entar -->$_COOKIE
-<img src="<?php get_template_directory_uri(); ?>/assets/img/restaurante.jpg" alt="Foto do restaurante">
+<!-- Apenas para testar se o cacete da imagem vai entrar -->
+<div class="container-img">
+<img src="<?= get_template_directory_uri(); ?>/assets/img/restaurante.jpg">
+</div>
+<div class="container-texto">
 <p>É um fato conhecido de todos que um leitor se distrairá com o conteúdo de texto legível de uma página quando estiver examinando sua diagramação. A vantagem de usar Lorem Ipsum é que ele tem uma distribuição normal de letras, ao contrário de "Conteúdo aqui, conteúdo aqui", fazendo com que ele tenha uma aparência similar a de um texto legível. Muitos softwares de publicação e editores de páginas na internet agora usam Lorem Ipsum como texto-modelo padrão, e uma rápida busca por 'lorem ipsum' mostra vários websites ainda em sua fase de construção. Várias versões novas surgiram ao longo dos anos, eventualmente por acidente, e às vezes de propósito (injetando humor, e coisas do gênero).</p>
-
+</div>
+</section>
 <?php get_footer(); ?>
