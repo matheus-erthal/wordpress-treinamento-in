33f8b4c61c4fb11f65a68fd65e700e0cc3a841d3
leticiavna <leticiavna@id.uff.br>
2018-01-19 21:54:56 -0200

    atualizando as modificações para as imagens e sections

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
-<?php 
+<?php
+/**
+ *
+ * @package WordPress
+ * @subpackage meu-tema
+ * @since 1.0
+ * @version 1.0
+ */
 
 get_header(); ?>
 
-<body>
 	<header class="top">
 		<div class="top-container">
-			<a class="top-logo" href="#"><img src="" alt="IN Junior"></a>
+			<a class="top-logo" href="#"><img src="<?= get_template_directory_uri(); ?>/img/logo-in.png" alt="IN Junior"></a>	
 			<nav class="menu-nav">
 				<ul>
 					<li><a href="#">Quem somos</a></li>
@@ -15,27 +21,29 @@ get_header(); ?>
 				</ul>
 			</nav>
 		</div>
-	</header>
-	<main class="intro">
+	</header> 
+	<section class="intro">
 		<p> Welcome to the IN experience </p>
-	</main>
+	</section>
 
 	<section class="principal">
 		<div class="quem-somos">
 			<h2> Quem Somos </h2>
-			<div class="wolf"> <img src="" alt="Lobo">
-			<p>  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean in metus eu lorem vulputate consectetur. Donec nec ultrices ante. Phasellus viverra orci in est suscipit, sed volutpat risus feugiat. Sed bibendum ut neque dapibus vestibulum. Aliquam erat volutpat. Duis pulvinar nunc quis auctor rhoncus. </p>
+			<div class="wolf"> 
+			<img src="<?= get_template_directory_uri(); ?>/img/lobo.png"alt="Lobo">
+			</div>
+				<p>  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean in metus eu lorem vulputate consectetur. Donec nec ultrices ante. Phasellus viverra orci in est suscipit, sed volutpat risus feugiat. Sed bibendum ut neque dapibus vestibulum. Aliquam erat volutpat. Duis pulvinar nunc quis auctor rhoncus. </p> <br>
 				<p> Mauris ac eros sit amet odio maximus mollis nec in tortor. Ut imperdiet fermentum malesuada. Vivamus a justo urna. Donec sit amet scelerisque mauris. Sed vitae vulputate velit, et rutrum mauris. Integer ac sagittis dolor, eget vehicula tortor. Phasellus rutrum sodales rhoncus. Suspendisse id diam diam. In hac habitasse platea dictumst. Morbi imperdiet tellus ut sapien bibendum, id sagittis odio aliquet. </p>
-				 </div>
+				 </div> 
 		</div>
 	</section>
 
 	<section class="projetos">
 		<div class="projects">
 			<h2> Projetos </h2>
-			<div class="slider">
+			<div class="slider"> 
 
-				<p>  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean in metus eu lorem vulputate consectetur. Donec nec ultrices ante. Phasellus viverra orci in est suscipit, sed volutpat risus feugiat. Sed bibendum ut neque dapibus vestibulum. Aliquam erat volutpat. Duis pulvinar nunc quis auctor rhoncus. </p>
+				<p>  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean in metus eu lorem vulputate consectetur. Donec nec ultrices ante. Phasellus viverra orci in est suscipit, sed volutpat risus feugiat. Sed bibendum ut neque dapibus vestibulum. Aliquam erat volutpat. Duis pulvinar nunc quis auctor rhoncus. </p> 
 				<p> Mauris ac eros sit amet odio maximus mollis nec in tortor. Ut imperdiet fermentum malesuada. Vivamus a justo urna. Donec sit amet scelerisque mauris. Sed vitae vulputate velit, et rutrum mauris. Integer ac sagittis dolor, eget vehicula tortor. Phasellus rutrum sodales rhoncus. Suspendisse id diam diam. In hac habitasse platea dictumst. Morbi imperdiet tellus ut sapien bibendum, id sagittis odio aliquet. </p>
 				<img src="" alt="projeto">
 			</div>
@@ -52,7 +60,7 @@ get_header(); ?>
 		<img src="" alt="Mapa do IC-UFF">
 		<br>
 		<p> Rua Passo da Pátria, 156 - Instituto de Computação - Sala 115-B. Niterói - RJ <br>
-			<i class="fa fa-phone"></i> (22) 99746-1629 <br>
+			<i class="fa fa-phone"></i> (22) 99746-1629 <br> 
 			<i class="fa fa-envelope"></i> contato@injunior.com.br</p>
 		<form>
 		<input type="text" placeholder="Digite seu nome"> <br>
@@ -60,9 +68,9 @@ get_header(); ?>
 		<input type="text" placeholder="Escreva..."> <br>
 		<button type="submit"><a href="#">Envia</a></button>
 		</form>
+
 	</section>
 
 
-	
-<?php get_footer(); ?>
 
+<?php get_footer();
--- single.php
+++ single.php
@@ -11,21 +11,10 @@
  */
 
 get_header(); ?>
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-=======
-=======
->>>>>>> 421097c6181a47c8466064ed77aed7a8f03b6b0f
-<?php while(have_posts()):  the_post();?>:
-  <h2><?php the_title();?></h2>
-  <h3><?php the_content();?></h3>
-
+	
+<?php while(have_posts()) : the_post(); ?>
+	<h2><?php the_title(); ?></h2>
+	<p><?php the_content(); ?></p>
 <?php endwhile; ?>
-<<<<<<< HEAD
->>>>>>> 421097c6181a47c8466064ed77aed7a8f03b6b0f
-=======
->>>>>>> 421097c6181a47c8466064ed77aed7a8f03b6b0f
 
 <?php get_footer();
