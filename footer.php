c09ec2d9cb8d443f3d449b0cd2b8e44c2df632fc
Bárbara <bramos@id.uff.br>
2018-01-19 14:23:13 -0200

    Atualizando CSS | Footer

--- footer.php
+++ footer.php
@@ -12,19 +12,14 @@
 
 <footer class="footer">
     <div class="footer-container">
-
-          <input class="barra" type="text" onblur="if (this.value == '')
-            {this.value = 'Pesquisar';}" onfocus="if
-            (this.value == 'Pesquisar')
-            {this.value = '';}" value="Pesquisar">
-          <input type="submit" class="botao-barra" value="<?php esc_attr_e( 'ir', 'nest' ); ?>">
-
-    <div class="footer-container-img">
+      <?php get_search_form(); ?>
+      <div class="footer-container-img">
             <a href="http://facebook.com"><img src="<?= get_template_directory_uri(); ?>/assets/img/facebook.png"></a>
             <a href="http://twitter.com"><img src="<?= get_template_directory_uri(); ?>/assets/img/twitter.png"></a>
             <a href="http://instagram.com"><img src="<?= get_template_directory_uri(); ?>/assets/img/instagram.png"></a>
-        </div>
+      </div>
     </div>
+    
     <div class="footer-sobre">
     <p>Layout desenvolvido pela equipe 1 Ⓒ 2018</p>
     </div>
---
+++ searchform.php
@@ -0,0 +1,7 @@
+
+<form action="/" method="get" accept-charset="utf-8"  role="search">
+  <div>
+    <input type="text" class="barra" placeholder="Pesquisar" value="<?php the_search_query(); ?>" />
+    <input type="submit" class="botao-barra" value="ir" />
+  </div>
+</form>
