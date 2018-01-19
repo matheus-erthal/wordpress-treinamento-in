fcd6be4adf6b2b24efa477e58bee5f4a76b62f47
Bárbara <bramos@id.uff.br>
2018-01-19 17:40:32 -0200

    Atualizando CSS | Quem Somos

--- quem-somos.php
+++ quem-somos.php
@@ -1,14 +1,19 @@
 <?php
-/* Template Name: Quem Somos */
+/* Template Name: Quem Somos*/
 get_header(); ?>
-<section class="container">
-<div class="container-fotos">
-<!-- Apenas para testar se o cacete da imagem vai entrar -->
-<div class="container-img">
-<img src="<?= get_template_directory_uri(); ?>/assets/img/restaurante.jpg">
-</div>
-<div class="container-texto">
-<p>É um fato conhecido de todos que um leitor se distrairá com o conteúdo de texto legível de uma página quando estiver examinando sua diagramação. A vantagem de usar Lorem Ipsum é que ele tem uma distribuição normal de letras, ao contrário de "Conteúdo aqui, conteúdo aqui", fazendo com que ele tenha uma aparência similar a de um texto legível. Muitos softwares de publicação e editores de páginas na internet agora usam Lorem Ipsum como texto-modelo padrão, e uma rápida busca por 'lorem ipsum' mostra vários websites ainda em sua fase de construção. Várias versões novas surgiram ao longo dos anos, eventualmente por acidente, e às vezes de propósito (injetando humor, e coisas do gênero).</p>
-</div>
+<section class="qs">
+  <div class="qs-container">
+      <!-- Apenas para testar se o cacete da imagem vai entrar -->
+    <div class="container-img">
+      <img src="<?= get_template_directory_uri(); ?>/assets/img/restaurante_1.png">
+    </div>
+    <div class="container-texto">
+      <p>Fundado em 1999 em pelos sócios Raul, Thales e Bárbara, a lanchonete foi o marco inicial no negócio do restaurante. Após uma reforma na casa onde foi à residência do avô paterno de Bárbara, transformou a mansão em um pequeno open mall, alugando suas salas e lojas. Na lojinha da esquina abriu a Lanchonete Reale. Ali nasceu como um negócio diferenciado, que logo despertou a atenção da população local, abrindo caminho para o que viria a ser o Restaurante Sabor Brasil.
+</br>A nossa culinária brasileira é rica, saborosa e diversificada. Cada um dos estados brasileiros tem seus pratos típicos, preparados de acordo com antigas tradições, que são transmitidas a cada geração.
+</br>Atualmente considerado o melhor restaurante de Niterói pelo Prêmio Rio Show de Gastronomia - Jornal O Globo, o Sabor Brasil sem pretensão de ser eloquente. Ao contrário, sua mensagem é sintática e pungente: sair da zona de conforto e propor uma nova experiência gastronômica, resgatando os sabores mais autênticos da cozinha brasileira sob um olhar tradicional.
+</br>A gastronomia brasileira é um sonho viável. Uma realidade genuína que reverbera por todo o mundo visando o cuidado com o seu legado, tradição, qualidade dos produtos oferecidos e excelência no atendimento aos seus clientes, objetivando a modernização da casa e novos projetos a caminho.
+</p>
+    </div>
+  </diV>
 </section>
 <?php get_footer(); ?>
