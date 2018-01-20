40cf20ef8b554e2a5e81781a9622294db7f7a7a6
Beatriz Tantow <beatriz.tantow@gmail.com>
2018-01-20 21:04:01 -0200

    Alteração na precisão do float com a média da nota na página calculadora.

--- js/home.js
+++ js/home.js
@@ -17,7 +17,7 @@ $(document).ready(function(){
         var nota1 = parseFloat($('#nota1').val());
         var nota2 = parseFloat($('#nota2').val());
         var nota3 = parseFloat($('#nota3').val());
-        var resultado = parseFloat((nota1 + nota2 + nota3) / 3);
+        var resultado = Math.round(parseFloat((nota1 + nota2 + nota3) / 3)*100)/100;
 		$('.media-final').text(resultado);
 		$('.mensagem').fadeIn(1000);
 		$('.assinatura').fadeIn(1000);
