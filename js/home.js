061decdd34005004ebb2a72cc8cbb657718f46ab
Beatriz Tantow <beatriz.tantow@gmail.com>
2018-01-20 21:11:48 -0200

    Alteração no home.js

--- js/home.js
+++ js/home.js
@@ -1,26 +1,25 @@
 $(document).ready(function(){
 	$('.back-to-top').fadeOut().hide();
-	console.log("batata")
-     $(window).scroll(function(){
-            if ($(this).scrollTop() > 300) {
-                $('.back-to-top').fadeIn();
-            } else {
-                $('.back-to-top').fadeOut();
-            }
-        });
-        $('.back-to-top').click(function(){
-          $('html').animate({scrollTop: 0}, 500);
-    })
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 300) {
+			$('.back-to-top').fadeIn();
+		} else {
+			$('.back-to-top').fadeOut();
+		}
+	});
+	
+	$('.back-to-top').click(function(){
+		$('html').animate({scrollTop: 0}, 500);
+	});
 
-
-    $(".calcular").click(function(){
-        var nota1 = parseFloat($('#nota1').val());
-        var nota2 = parseFloat($('#nota2').val());
-        var nota3 = parseFloat($('#nota3').val());
-        var resultado = Math.round(parseFloat((nota1 + nota2 + nota3) / 3)*100)/100;
+	$(".calcular").click(function(){
+		var nota1 = parseFloat($('#nota1').val());
+		var nota2 = parseFloat($('#nota2').val());
+		var nota3 = parseFloat($('#nota3').val());
+		var resultado = Math.round(parseFloat((nota1 + nota2 + nota3) / 3)*100)/100;
 		$('.media-final').text(resultado);
 		$('.mensagem').fadeIn(1000);
 		$('.assinatura').fadeIn(1000);
-    });
+	});
 
 });
