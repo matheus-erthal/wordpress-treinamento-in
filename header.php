1d5bc12f6bcf367c1d7eef990e68a2e379870b3c
ferreira-mariana <ferreira_mariana@id.uff.br>
2018-01-20 17:27:52 -0200

    consertando a logo do menu

--- header.php
+++ header.php
@@ -32,7 +32,7 @@
 
 	<header class="menu">
 		<div class="menu-container">
-			<a class="menu-logo" href="#"><img src="img/logo-branca-in.png" alt="logo branca da In Junior"></a>	
+			<img src="<?= get_template_directory_uri(); ?>/img/logo-branca-in.png" alt="logo branca da In Junior">	
 			<nav class="menu-nav">
 				<ul>
 					<li><a href="#">Quem somos</a></li>
