a8a3d85f4ad60a32951d0f26e78f2974760289b3
Samiry <samirymattos@id.uff.br>
2018-01-21 16:47:06 -0200

    melhorando funções da página aviso

--- js/home.js
+++ js/home.js
@@ -22,27 +22,7 @@ $(document).ready(function(){
 		$('.assinatura').fadeIn(1000);
 	});
 
-//accordion dos avisos
-	var acc = document.getElementsByClassName("accordion");
-	var i;
-
-	for (i = 0; i < acc.length; i++) {
-	    acc[i].addEventListener("click", function() {
-	        /* Toggle between adding and removing the "active" class,
-	        to highlight the button that controls the panel */
-	        this.classList.toggle("active");
-
-	        /* Toggle between hiding and showing the active panel */
-	        var panel = this.nextElementSibling;
-	        if (panel.style.display === "block") {
-	            panel.style.display = "none";
-	        } else {
-	            panel.style.display = "block";
-	        }
-	    });
-	}
-
-/* accordion avisos */
+/* accordion avisos
 
 	var acc = $(".painel-botao");
 	var i;
@@ -63,5 +43,10 @@ $(document).ready(function(){
 			}
 		});
 	};
+	**/
+	$('.painel').click(function(){
+		$('.painel-container').slideUp();
+		$(this).find(".painel-container").slideToggle();
 
+	});
 });
