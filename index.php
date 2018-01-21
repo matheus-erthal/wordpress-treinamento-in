618b83f6378e76788e63249f56a519b2249b1e1e
Matheus Erthal <matheusamancio@id.uff.br>
2018-01-21 01:45:51 +0000

    Zerando a Index.php

--- index.php
+++ index.php
@@ -9,15 +9,7 @@
 
 get_header(); ?>
 
-<h1 style="text-align: center">Site do Amanco</h1>
 
-<?php $query_notas = new WP_Query(array('post_type' => notas)) ?>
-
-<?php if($query_notas->have_posts()): 
-    while($query_notas->have_posts()): $query_notas->the_post(); ?>
-        <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-<?php endwhile;
-    endif; ?>
 
 
 <?php get_footer();
