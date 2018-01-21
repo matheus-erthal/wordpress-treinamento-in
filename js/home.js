d9d2c1378113ef6951a3ee995e31e8067388c9d2
Beatriz Tantow <beatriz.tantow@gmail.com>
2018-01-21 15:15:31 -0200

    Corrigindo erro de português.

--- js/home.js
+++ js/home.js
@@ -19,7 +19,7 @@ $(document).ready(function(){
 		if (isNaN(nota1) || isNaN(nota2) || isNaN(nota3)){
 			$('.media-final').text("Preencha corretamene os campos");
 		} else if(nota1 < 0 || nota1 > 10 || nota2 < 0 || nota2 > 10 || nota3 < 0 || nota3 > 10){
-			$('.media-final').text("As notas deve estar todas entre zero e dez.");
+			$('.media-final').text("As notas devem estar todas entre zero e dez.");
 		} else{
 			var resultado = Math.round(parseFloat((nota1 + nota2 + nota3) / 3)*100)/100;
 			$('.media-final').text("Sua média é " + resultado + ".");
