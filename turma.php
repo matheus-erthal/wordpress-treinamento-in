dcfbbb1b2566aff2bc6c8c3865a946c3256b9855
Samiry <samirymattos@id.uff.br>
2018-01-21 18:24:12 -0200

    resolvendo erro em arq class-wp-hook.php

--- turma.php
+++ turma.php
@@ -9,48 +9,53 @@
  */
 
 get_header(); ?>
-<?php if (have_posts()): ?>
-    <?php while(have_posts()): ?>
-      <img src=<?php the_field('foto_turma') ?> alt="Foto da Turma"></img>
-      <?php the_post(); ?>
-      <?php endwhile; ?>
-<?php endif; ?>
-
-<?php $query_posts = new WP_Query(array('posts_per_page' => 3,'category_name' => 'avisos')) ?>
-    <?php if ($query_posts->have_posts()): ?>
-        <?php while($query_posts->have_posts()): $query_posts->the_post(); ?>
-            <div class="painel">
-                <div class="painel-botao">
-                        <div class="painel-data">
-                          <?php  the_time("F j, Y"); ?> <!-- data -->
-                        </div>
-                        <div class="painel-titulo">
-                          <?php the_title(); ?> <!-- titulo do aviso -->
-                        </div>
-                </div>
-                <div class="painel-container">
-                    <div class="painel-mens">
-                      <?php the_content(); ?> <!-- conteudo da noticia -->
-                    </div>
-                    <div class="painel-para">
-                      <?php the_field('para_mensagem'); ?> <!-- para quem -->
-                    </div>
-                </div>
-            </div>
-      <?php endwhile; ?>
-<?php endif; ?>
-
-  <?php if (have_posts()): ?> <!-- Ainda não funciona, vou resolver mais tarde com Amanco -->
-      <?php while(have_posts()): ?>
-        <h2> CARALHOOOOOOOOOO </h2>
-            <?php the_post(); ?>
-            <?php the_field('alunos'); ?> <!-- Alunos -->
-            <?php the_field('descricao'); ?> <!-- Descricao -->
-            <?php the_field('arquivos'); ?> <!-- Arquivos -->
-
+<div class="turma">
+    <div class="turma-topo">
+      <?php if (have_posts()): ?>
+        <?php while(have_posts()): ?>
+          <img src="<?php the_field('foto_turma') ?>" alt="Foto da Turma"></img>
+          <?php the_post(); ?>
         <?php endwhile; ?>
-  <?php endif; ?>
+      <?php endif; ?>
+    </div>
 
+    <?php $query_posts = new WP_Query(array('posts_per_page' => 3,'category_name' => 'avisos')) ?>
+        <?php if ($query_posts->have_posts()): ?>
+            <div class="turma-avisos">
+                <?php while($query_posts->have_posts()): $query_posts->the_post(); ?>
+                    <div class="painel">
+                        <div class="painel-botao">
+                                <div class="painel-data">
+                                  <?php  the_time("F j, Y"); ?> <!-- data -->
+                                </div>
+                                <div class="painel-titulo">
+                                  <?php the_title(); ?> <!-- titulo do aviso -->
+                                </div>
+                        </div>
+                        <div class="painel-container">
+                            <div class="painel-mens">
+                              <?php the_content(); ?> <!-- conteudo da noticia -->
+                            </div>
+                            <div class="painel-para">
+                              <?php the_field('para_mensagem'); ?> <!-- para quem -->
+                            </div>
+                        </div>
+                    </div>
+                <?php endwhile; ?>
+            </div>
+    <?php endif; ?>
+
+      <?php if (have_posts()): ?> <!-- Ainda não funciona, vou resolver mais tarde com Amanco -->
+          <?php while(have_posts()): ?>
+            <h2> CARALHOOOOOOOOOO </h2>
+                <?php the_post(); ?>
+                <?php the_field('alunos'); ?> <!-- Alunos -->
+                <?php the_field('descricao'); ?> <!-- Descricao -->
+                <?php the_field('arquivos'); ?> <!-- Arquivos -->
+
+            <?php endwhile; ?>
+      <?php endif; ?>
+</div>
 
   <button class="back-to-top" id="topo">Suba ao topo</button>
 <?php get_footer();
