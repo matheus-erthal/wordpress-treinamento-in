b803062cd411ed57dd011364cd26186c71d76343
ThalesMachado <thalesm@id.uff.br>
2018-01-22 10:07:46 -0200

    Fim do sprint 4

--- index.php
+++ index.php
@@ -12,20 +12,25 @@
 
 <?php
 $nossa_query = new WP_Query( array( 'post_type' => 'receitas') );
-$max_postagens = 0;?>
+?>
 
 
 <?php if ($nossa_query->have_posts()): ?>
-    <?php while($nossa_query->have_posts() and $max_postagens < 3): ?>
+    <?php while($nossa_query->have_posts()): ?>
         <?php $nossa_query->the_post(); $max_postagens++;?>
-        <div class="conteudo">
-        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-        <br>
-        <img src="<?php the_field('foto_da_receita'); ?>    ">
-        <br>
+        <section class="ph">
+    <div class="ph-container">
+    <div class="ph-container-texto">
+      <p>
+        <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+        <div class="ph-container-blue">
+        <img class ="ph-container-imagens" src="<?php the_field('foto_da_receita'); ?>">
+        </div>
         <p><?php the_content() ?></p>
-
-    </div>
+      </p>
+    
+    </diV>
+    </section>
 
     <?php endwhile; ?>
 <?php endif; ?>
