a85f4102aa7d8cbbce118db09d270756134787d0
ThalesMachado <thalesm@id.uff.br>
2018-01-22 14:04:00 -0200

    Finalização do CSS na home e na singl

--- index.php
+++ index.php
@@ -11,27 +11,55 @@
  get_header(); ?>
 
 <?php
-$nossa_query = new WP_Query( array( 'post_type' => 'receitas') );
-?>
+$nossa_query = new WP_Query( array( 'post_type' => 'receitas') );?>
 
 
 <?php if ($nossa_query->have_posts()): ?>
     <?php while($nossa_query->have_posts()): ?>
-        <?php $nossa_query->the_post(); $max_postagens++;?>
-        <section class="ph">
-    <div class="ph-container">
-    <div class="ph-container-texto">
-      <p>
-        <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
-        <div class="ph-container-blue">
-        <img class ="ph-container-imagens" src="<?php the_field('foto_da_receita'); ?>">
-        </div>
-        <p><?php the_content() ?></p>
-      </p>
-    
-    </diV>
-    </section>
+        <?php $nossa_query->the_post();?>
+        <div class="ph">
+            <div class="ph-container">
+             <img src="<?php the_field('foto_da_receita'); ?>" class="ph-container-blue">
+        
+        
+            <div class="ph-container-texto">
+             <p>
+                <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+                 <?php the_content() ?>
+            </p>
+            </div>
+            </div>
+        
 
+        <?php $nossa_query->the_post();?>
+        
+            <div class="ph-container">
+             <img src="<?php the_field('foto_da_receita'); ?>" class="ph-container-yellow">
+        
+        
+            <div class="ph-container-texto">
+             <p>
+                <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+                 <?php the_content() ?>
+            </p>
+            </div>
+            </div>
+        
+
+        <?php $nossa_query->the_post();?>
+        
+            <div class="ph-container">
+             <img src="<?php the_field('foto_da_receita'); ?>" class="ph-container-green">
+        
+        
+            <div class="ph-container-texto">
+             <p>
+                <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+                 <?php the_content() ?>
+            </p>
+            </div>
+            </div>
+        </div>
     <?php endwhile; ?>
 <?php endif; ?>
 
--- single.php
+++ single.php
@@ -3,12 +3,10 @@
 
 <?php while(have_posts()) : the_post(); ?>
     <div class="postagem">
-    <h2 class="titulo">
-        <a href="<?php the_permalink()?>">
-            <bold><?php the_title(); ?></bold>
-    </h2></a>
-    <p class="conteudo"><?php the_content() ?></p>
-   <img src="<?php the_field('foto_da_receita') ?>">
+    <img src="<?php the_field('foto_da_receita') ?>">
+    <h2><a href="<?php the_permalink()?>"><?php the_title(); ?></a></h2>
+    <p><?php the_content() ?></p>
+   
     <p>
         <?php the_field('ingredientes') ?>
     </p>
