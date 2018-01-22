e36765e81ce9df00a7e7ed5a07cbf5339d801e00
Bárbara <bramos@id.uff.br>
2018-01-22 12:53:02 -0200

    Fim do Último Sprint | Ler a READ ME

--- index.php
+++ index.php
@@ -1,40 +1,160 @@
 <?php
-/**
- *
- * @package WordPress
- * @subpackage meu-tema
- * @since 1.0
- * @version 1.0
- */
-
- 
- get_header(); ?>
-
-<?php
-$nossa_query = new WP_Query( array( 'post_type' => 'receitas') );?>
+/* Template Name: Home Template */
+get_header(); ?>
 
 
-<?php if ($nossa_query->have_posts()): ?>
-    <?php while($nossa_query->have_posts()): ?>
-        <?php $nossa_query->the_post(); ?>
-        <div class="conteudo">
-        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-        <br>
-        <img src="<?php the_field('foto_da_receita'); ?>    ">
-        <br>
-        <p><?php the_content() ?></p>
 
+
+<section class="ph">
+
+  <div class="ph-container-imagens">
+    <div class="ph-container-blue">
+        <img src="<?= get_template_directory_uri(); ?>/assets/img/bobo.jpg" >
     </div>
+    <div class="ph-container-green">
+        <img src="<?= get_template_directory_uri(); ?>/assets/img/Rabada.jpg" >
+    </div>
+    <div class="ph-container-yellow">
+        <img src="<?= get_template_directory_uri(); ?>/assets/img/feijoada.png">
+    </div>
+    </div>
+    <div class="ph-container">
+    <div class="ph-container-texto">
+      <p>
+        <h1>Bobó de Camarão</h1>
+        <h2>Ingredientes:</h2>
+        <ul>
+          <li>  1 ½ kg de camarões médios limpos </li>
+          <li>  Sal a gosto </li>
+          <li>  Pimenta </li>
+          <li>  Suco de 2 limões </li>
+          <li>  1 maço de cheiro verde picado </li>
+          <li>  1 folha de louro verde picada </li>
+          <li>  2 cebolas raladas </li>
+          <li>  5 colheres (sopa) de óleo </li>
+          <li>  2 pimentões sem pele picados </li>
+          <li>  8 tomates sem pele nem semente </li>
+          <li> 1 kg de aipim cozido </li>
+          <li>  ½ kg de inhame </li>
+          <li>  2 copos de leite de coco </li>
+          <li>  4 colheres (sopa) de azeite de dendê </li>
+          <li>  Molho de pimenta vermelha a gosto </li>
+        </ul>
 
-    <?php endwhile; ?>
-<?php endif; ?>
+        <h2>Modo de preparo:</h2>
+        <ul>
+          <li>Tempere o camarão com sal, pimenta, suco de limão, cheiro verde, louro e cebola </li>
+          <li>Deixe a mistura tomar gosto por cerca de 30 minutos</li>
+          <li>Refogue o camarão no óleo, juntamente com os temperos</li>
+          <li>Acrescente o pimentão e o tomate</li>
+          <li>Tampe e deixe apurar em fogo baixo durante 15 minutos</li>
+          <li>Cozinhe o aipim e o inhame, bata no liquidificador com o leite de coco</li>
+          <li>Misture ao refogado de camarão e deixe apurar mais um pouco</li>
+          <li>Acrescente o azeite de dendê, molho de pimenta, mexa e deixe mais 5 minutos</li>
+        </ul>
+          <h1>R$ 49,90</h1>
+      </p>
+    </div>
+  </diV>
 
 
-<?php if(have_posts()): ?>
-    <?php while(have_posts()): the_post(); ?>
-        <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-<?php endwhile; ?>
-    <?php endif; ?>
 
+    <div class="ph-container">
+    <div class="ph-container-texto">
+      <p>
+        <h1>Feijoada</h1>
+        <h2>Ingredientes:</h2>
+        <ul>
+            <li>1/2 kg de feijão preto de boa qualidade</li>
+            <li>1/2 kg de carne seca de boa qualidade (com pouca gordura e muita carne)</li>
+            <li>1/2 kg de lombo salgado</li>
+            <li>1/2 kg de costela salgada</li>
+            <li>1/2 kg de bacon de boa qualidade (com pouca gordura e muita carne )</li>
+            <li>4 linguiças calabresas defumadas (Sadia ou Perdigão)</li>
+            <li>4 cebolas grandes picadas bem pequenas</li>
+            <li>6 cabeças de alho trituradas ou socadas bem pequenas</li>
+          </ul>
+
+          <h2>Couve:</h2>
+          <ul>
+            <li>2 moles de couve cortada bem fininha</li>
+            <li>2 cabeças de alho</li>
+          </ul>
+
+          <h2>Molho vinagrete:</h2>
+          <ul>
+            <li>2 cebolas grandes cortadas bem pequenas</li>
+            <li>2 tomates grandes cortados pequenos</li>
+            <li>2 pimentões</li>
+            <li>2 moles de salsinha cortado pequeno</li>
+            <li>250 ml de suco de limão (1 copo de requeijão)</li>
+            <li>250 ml de azeite de oliva</li>
+            <li>100 ml de vinagre</li>
+            <li>Sal a gosto</li>
+          </ul>
+
+            <h2>Modo de preparo:</h2>
+            <ul>
+              <li>Escolha e lave o feijão, coloque de molho por no mínimo 1 hora antes de cozinhar</li>
+              <li>Cozinhe com a mesma água em uma panela de pressão, com água até a marca da panela, por 20 minutos</li>
+              <li>Escalde bem os salgados até retirar o excesso de sal</li>
+              <li>Abra a panela, coloque os salgados e deixe cozinhar por mais 20 minutos</li>
+              <li>Apague o fogo e reserve</li>
+              <li>Refogue no azeite o alho e a cebola, tempere o feijão com sal a gosto</li>
+            </ul>
+
+            <h2>Farofa:</h2>
+            <ul>
+                <li>Frite a linguiça em óleo de soja</li>
+                <li>Coloque os 2 envelopes de sazón e a farinha</li>
+                <li>Misture bem e apague o fogo (não coloque sal pois o sazón já tem)</li>
+            </ul>
+            <h2>Couve:</h2>
+            <ul>
+              <li>Frite o alho no azeite, quando estiver bem douradinho jogue a couve, refogue com sal a gosto e apague o fogo</li>
+            </ul>
+
+            <h2>Molho vinagrete:</h2>
+            <ul>
+              <li>Misture a cebola, o tomate, o pimentão, a salsinha, o suco de limão, o azeite, o vinagre e sal a gosto</li>
+              <li>Sirva com arroz branco, laranjas e uma caipirinha</li>
+            </ul>
+            <h1>R$ 35,90</h1>
+      </p>
+    </div>
+  </div>
+
+
+
+    <div class="ph-container">
+    <div class="ph-container-texto">
+      <p>
+        <h1>Rabada</h1>
+        <h2>Ingredientes:</h2>
+        <ul>
+          <li> 2 kg de rabada, em pedaços</li>
+          <li>1/2 kg de batatas</li>
+          <li>2 cebolas picadas</li>
+          <li>1 molho de cheiro verde</li>
+          <li>3 colheres (sopa) de óleo</li>
+          <li>3 pingos de pimenta malagueta</li>
+          <li>tempero mineiro a gosto</li>
+          <li>4 tomates pelados</li>
+        </ul>
+
+        <h2>Modo de preparo:</h2>
+        <ul>
+          <li>Lavar os pedaços de rabada, removendo as gorduras e a pele. Colocar o temperar e a pimenta.</li>
+          <li>Levar ao fogo uma panela com o óleo, aquecer.</li>
+          <li>Adicionar os pedaços de rabada deixando-os cozer até corar. Tirar o excesso de gordura e adicionar água, tomates, cebola e pimenta.</li>
+          <li>Quando estiver quase cozida, juntar as batatas partidas ao meio e descascadas, deixando cozinhar bem com a panela tampada.</li>
+          <li>Juntar o cheiro verde na hora de servir.</li>
+        </ul>
+        <h1>R$ 29,50</h1>
+      </p>
+    </div>
+  </diV>
+
+</section>
 
 <?php get_footer(); ?>
