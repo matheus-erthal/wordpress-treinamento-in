f3d3d583256a79cb431a733b218cdd745bacb1fd
Maria Mussi <mariamussi@id.uff.br>
2018-01-21 23:25:00 -0200

    Adicionando diretório de js ao projeto

---
+++ js/inburger.js
@@ -0,0 +1,42 @@
+// slider
+$(document).ready(function(){	
+	
+	function slider(sliderName, velocidade) {
+	  var sliderClass = '.' + sliderName,
+			  activeClass = 'active',
+			  rotate = setInterval(rotateSlide, velocidade);
+  
+	  $(sliderClass + ' > :first').addClass(activeClass);
+  
+	  $(sliderClass).hover(function(){
+		  clearInterval(rotate);
+	  }, function() {
+		  rotate = setInterval(rotateSlide, velocidade);
+	  });
+  
+	  function rotateSlide() {
+		  var activeSlide = $(sliderClass + ' > .' + activeClass),
+				  nextSlide = activeSlide.next();
+  
+		  if(nextSlide.length == 0) {
+			  nextSlide = $(sliderClass + ' > :first');
+		  }
+		  activeSlide.removeClass(activeClass);
+		  nextSlide.addClass(activeClass);
+	  }
+  }
+  
+  slider('intro', 2500);
+  
+  //fadein
+  
+	  $("#img1").mouseover(function(){
+		  $("#botao1").addClass("aparecer");
+	  });
+
+	  $("#img2").mouseover(function(){
+		$("#botao2").addClass("aparecer");
+	});
+  });
+		
+
