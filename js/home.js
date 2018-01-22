8439daff24828cc90f3e3e0794b02b2b865f7889
Samiry <samirymattos@id.uff.br>
2018-01-22 13:31:11 -0200

    consertando js, add layout turmas

--- js/home.js
+++ js/home.js
@@ -13,19 +13,11 @@ $(document).ready(function(){
 	});
 
 	$(".calcular").click(function(){
-		var nota1 = parseFloat($('#nota1').val().replace(",","."));
-		var nota2 = parseFloat($('#nota2').val().replace(",","."));
-		var nota3 = parseFloat($('#nota3').val().replace(",","."));
-		if (isNaN(nota1) || isNaN(nota2) || isNaN(nota3)){
-			$('.media-final').text("Preencha corretamene os campos");
-		} else if(nota1 < 0 || nota1 > 10 || nota2 < 0 || nota2 > 10 || nota3 < 0 || nota3 > 10){
-			$('.media-final').text("As notas devem estar todas entre zero e dez.");
-		} else{
-			var resultado = Math.round(parseFloat((nota1 + nota2 + nota3) / 3)*100)/100;
-			$('.media-final').text("Sua média é " + resultado + ".");
-		}
-		$('.mensagem').hide();
-		$('.assinatura').hide();
+		var nota1 = parseFloat($('#nota1').val());
+		var nota2 = parseFloat($('#nota2').val());
+		var nota3 = parseFloat($('#nota3').val());
+		var resultado = Math.round(parseFloat((nota1 + nota2 + nota3) / 3)*100)/100;
+		$('.media-final').text(resultado);
 		$('.mensagem').fadeIn(1000);
 		$('.assinatura').fadeIn(1000);
 	});
@@ -40,31 +32,15 @@ $(document).ready(function(){
 	        to highlight the button that controls the panel */
 	        this.classList.toggle("active");
 
-$(".calcular").click(function(){
-	var nota1 = parseFloat($('#nota1').val());
-	var nota2 = parseFloat($('#nota2').val());
-	var nota3 = parseFloat($('#nota3').val());
-	var resultado = Math.round(parseFloat((nota1 + nota2 + nota3) / 3)*100)/100;
-	$('.media-final').text(resultado);
-	$('.mensagem').fadeIn(1000);
-	$('.assinatura').fadeIn(1000);
-});
-function openCity(event, cityName) {
-  // Declare all variables
-	var i, tabcontent, tablinks;
-	// Get all elements with class="tabcontent" and hide them
-  tabcontent = document.getElementsByClassName("tabcontent");
-	for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-// Get all elements with class="tablinks" and remove the class "active"
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-  	tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-// Show the current tab, and add an "active" class to the button that opened the tab
-  document.getElementById(cityName).style.display = "block";
-  evt.currentTarget.className += " active";
+	        /* Toggle between hiding and showing the active panel */
+	        var panel = this.nextElementSibling;
+	        if (panel.style.display === "block") {
+	            panel.style.display = "none";
+	        } else {
+	            panel.style.display = "block";
+	        }
+	    });
+	}
 
 /* accordion avisos */
 
@@ -87,10 +63,5 @@ function openCity(event, cityName) {
 			}
 		});
 	};
-	**/
-	$('.painel').click(function(){
-		$('.painel-container').slideUp();
-		$(this).find(".painel-container").slideToggle();
 
-	});
 });
